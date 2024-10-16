75b4a28fd76e09e6aabf56b03c4c2191da9d078b
Adriano Messias <asmessias@gmail.com>
2024-10-16 14:31:52 -0300

    Configuração Herd e Sail.

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,17 +12,17 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        /*
+        
         $contato = new SiteContato();
 
         $contato->nome = 'Sistema';
         $contato->telefone = '(61) 99999-9999';
         $contato->email = 'contato@sg.com.br';
-        $contato->motivo = 1;
+        $contato->motivo_contatos_id = 1;
         $contato->mensagem = 'Seja bem-vindo ao sistema SG';
 
         $contato->save();
-        */
-        factory(SiteContato::class, 100)->create();
+        
+        //factory(SiteContato::class, 100)->create();        
     }
 }
